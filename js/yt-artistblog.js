5270e27b93b21a4658c64752c75b357e52b870a6
chris920 <chris920@gmail.com>
2014-05-31 18:09:26 -0700

    JS ~ Added page About & Register views
    Former-commit-id: 1e68dff8ab16fec32438880bb451e42ee5decda2

--- js/yt-artistblog.js
+++ js/yt-artistblog.js
@@ -9,12 +9,18 @@ var App = new (Backbone.View.extend({
 			e.preventDefault();
 			Backbone.history.navigate(e.target.pathname, {trigger: true});
 			$("html, body").animate({ scrollTop: 0 }, 200);
+		},
+		//above target.pathname does not work for buttons....
+		"click [href='/about']": function(e){
+			e.preventDefault();
+			Backbone.history.navigate('/about', {trigger: true});
+			$("html, body").animate({ scrollTop: 0 }, 200);
 		}
 	},
 	start: function(){
 		Backbone.history.start({pushState: true, root: '/'});
 	}
-}))({el: '.app'});
+}))({el: document.body});
 
 
 ///////// Models
@@ -50,7 +56,24 @@ App.Views.Intro = Backbone.View.extend({
 		return this;
 	}
 });
-App.Views.About = Backbone.View.extend({});
+App.Views.About = Backbone.View.extend({
+	id: 'aboutPage',
+	template: _.template($("#aboutTemplate").html()),
+	render: function(){
+		var html = this.template();
+		$(this.el).append(html);
+		return this;
+	}
+});
+App.Views.Register = Backbone.View.extend({
+	id: 'register',
+	template: _.template($("#registerTemplate").html()),
+	render: function(){
+		var html = this.template();
+		$(this.el).append(html);
+		return this;
+	}
+});
 App.Views.FeaturedArtist = Backbone.View.extend({});
 
 
@@ -59,10 +82,12 @@ App.Views.FeaturedArtists = Backbone.Collection.extend({});
 
 
 ///////// Routers
-App.AppRouter = new (Backbone.Router.extend({
+App.Router = new (Backbone.Router.extend({
 	routes: {
 		"": 	  			"index",
-		"uname":   			"showArtist"
+		"uname":   			"showArtist",
+		"about":   			"about",
+		"register":         "register"
 	},
 	initialize: function(){
 
@@ -76,6 +101,15 @@ App.AppRouter = new (Backbone.Router.extend({
 		var artistProfile = new App.Views.ArtistProfile();
 		$('.app').html(artistProfile.render().el);
 		hideMore(); //prototype load more
+	},
+	about: function(){
+		var about = new App.Views.About();
+		$('.app').html(about.render().el);
+	}
+	,
+	register: function(){
+		var register = new App.Views.Register();
+		$('.app').html(register.render().el);
 	}
 }));
 
