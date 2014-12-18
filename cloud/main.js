29ecd6032e797c8de57791bb0231a1b7a9e6ea8e
chris920 <chris920@gmail.com>
2014-12-18 15:10:02 -0800

    update before save tattoo cloud sync artist books
    with books

--- cloud/main.js
+++ cloud/main.js
@@ -242,63 +242,82 @@ Parse.Cloud.beforeSave("Tattoo", function(request, response) {
   var user = request.user;
   var tattoo = request.object;
 
-  if (!tattoo.dirty("file")) {
+  if (!tattoo.existed()) {
+
+    var userACL = new Parse.ACL(user);
+    userACL.setRoleWriteAccess("Admin",true);
+    userACL.setPublicReadAccess(true);
+    tattoo.setACL(userACL);
+
+    Parse.Cloud.httpRequest({
+      url: tattoo.get("file").url()
+    }).then(function(response) {
+      var image = new Image();
+      return image.setData(response.buffer);
+    }).then(function(image) {
+      var width = Math.min(image.height() * 0.75, image.width());   
+      var height = Math.min(image.width() * 1.33, image.height());
+      if (width < 369 ) {
+        return Parse.Promise.error("Choose a bigger image.");;
+      }
+      return image.crop({
+        left: (image.width() - width) / 2,
+        top: (image.height() - height) / 2,
+        width: width,
+        height: height
+      });
+    }).then(function(image) {
+      tattoo.image = image
+      return image.scale({ width: 369, height: 493 }); 
+    }).then(function(image) {
+      return image.setFormat("JPEG");
+    }).then(function(image) {
+      return image.data();
+    }).then(function(buffer) {
+      var base64 = buffer.toString("base64");
+      var cropped = new Parse.File("thumbnail.jpg", { base64: base64 });
+      return cropped.save();
+    }).then(function(cropped) {
+      tattoo.set("fileThumb", cropped);
+    }).then(function() {
+      return tattoo.image.scale({ width: 124, height: 166 });
+    }).then(function(image) {
+      return image.setFormat("JPEG");
+    }).then(function(image) {
+      return image.data();
+    }).then(function(buffer) {
+      var base64 = buffer.toString("base64");
+      var cropped = new Parse.File("thumbnail.jpg", { base64: base64 });
+      return cropped.save();
+    }).then(function(cropped) {
+      tattoo.set("fileThumbSmall", cropped);
+    }).then(function(result) {
+      response.success();
+    }, function(error) {
+      response.error(error);
+    });
+
+  } else if (tattoo.dirty('artistBooks')) {
+    var oldBooks = tattoo.get('oldArtistBooks');
+    var newBooks = tattoo.get('artistBooks');
+    var removed = _.difference(oldBooks, newBooks);
+    var added = _.difference(newBooks, oldBooks);
+
+    //TODO ~ redo to pass the tattoo and reduce the extra query
+    editBooks({params: {added: added, removed: removed, tattooId: tattoo.id}}, {
+      success: function(result) {
+        tattoo.set('oldArtistBooks', newBooks);
+        response.success();
+      },
+      error: function(error) {
+        console.log(error);
+        response.error();
+      }
+    });
+  } else {
     response.success();
-    return;
   }
 
-  var userACL = new Parse.ACL(user);
-  userACL.setRoleWriteAccess("Admin",true);
-  userACL.setPublicReadAccess(true);
-  tattoo.setACL(userACL);
-
-  Parse.Cloud.httpRequest({
-    url: tattoo.get("file").url()
-  }).then(function(response) {
-    var image = new Image();
-    return image.setData(response.buffer);
-  }).then(function(image) {
-    var width = Math.min(image.height() * 0.75, image.width());   
-    var height = Math.min(image.width() * 1.33, image.height());
-    if (width < 369 ) {
-      return Parse.Promise.error("Choose a bigger image.");;
-    }
-    return image.crop({
-      left: (image.width() - width) / 2,
-      top: (image.height() - height) / 2,
-      width: width,
-      height: height
-    });
-  }).then(function(image) {
-    tattoo.image = image
-    return image.scale({ width: 369, height: 493 }); 
-  }).then(function(image) {
-    return image.setFormat("JPEG");
-  }).then(function(image) {
-    return image.data();
-  }).then(function(buffer) {
-    var base64 = buffer.toString("base64");
-    var cropped = new Parse.File("thumbnail.jpg", { base64: base64 });
-    return cropped.save();
-  }).then(function(cropped) {
-    tattoo.set("fileThumb", cropped);
-  }).then(function() {
-    return tattoo.image.scale({ width: 124, height: 166 });
-  }).then(function(image) {
-    return image.setFormat("JPEG");
-  }).then(function(image) {
-    return image.data();
-  }).then(function(buffer) {
-    var base64 = buffer.toString("base64");
-    var cropped = new Parse.File("thumbnail.jpg", { base64: base64 });
-    return cropped.save();
-  }).then(function(cropped) {
-    tattoo.set("fileThumbSmall", cropped);
-  }).then(function(result) {
-    response.success();
-  }, function(error) {
-    response.error(error);
-  });
 });
 
 Parse.Cloud.beforeDelete("Tattoo", function(request, response) {
